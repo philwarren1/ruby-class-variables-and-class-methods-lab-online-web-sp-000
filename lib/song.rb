cea9435e83b1a093d31de36189a607c7f85f5884
Philip Warren <philwarren1@gmail.com>
2020-08-09 15:22:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,14 @@ class Song
    attr_accessor :name, :artist, :genre
    
    @@count = 0
-   
+   @@genres = []
+    @@artists = []
    
    def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
     @@count += 1
-    @@genres = []
-    @@artists = []
     @@artists << @artist
     @@genres << @genre
   end
